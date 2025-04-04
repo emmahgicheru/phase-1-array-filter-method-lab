b9d9df708b09da9fbd724c074c0e98e6d7d8defe
Emmah Gicheru <gicheruemmah@gmail.com>
2025-04-05 02:33:30 +0300

    Completed the Array Filter lab code grade

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution here
+// Func 1: findMatching
+
+function findMatching(drivers, name) {
+
+  // Drivers whose name matches the given name (case insensitive)
+
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+// Func 2: fuzzyMatch
+
+function fuzzyMatch(drivers, letters) {
+
+  // Using filter method to find drivers whose name starts with the provided letters
+
+  return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+// Func 3: matchName
+
+function matchName(drivers, name) {
+
+  // Using filter to find drivers whose name matches the given name property in driver object
+
+  return drivers.filter(driver => driver.name === name);
+}
+
+const drivers = [
+  "Llewellyn", "Leon", "Leilani", "Alex"
+];
+
+// sample
+
+const driverObjects = [
+  { name: "Llewellyn", homecounty: "Kiambu" },
+  { name: "Leilani", homecounty: "Nairobi" },
+  { name: "Leon", homecounty: "Nakuru" },
+];
+
+// Testing the functions
+console.log(findMatching(drivers, "Llewellyn"));
+console.log(fuzzyMatch(drivers, "Leilani"));
+console.log(matchName(driverObjects, "Leon"));
